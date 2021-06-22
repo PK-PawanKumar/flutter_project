2f81741b53b966c68f0211034fff0c6668875e90
Pawan Kumar <pawan.kumar.ece20@itbhu.ac.in>
2021-06-22 20:52:58 +0530

    Chapter-5 end

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,103 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My First App",
-      home: HomePage(),
+      title: "Loginapp",
+      home: const LoginPage(),
       theme: ThemeData(
         primarySwatch: Colors.green,
-        // brightness: Brightness.light,
-        brightness: Brightness.dark,
-        accentColor: Colors.deepOrange,
+        brightness: Brightness.light,
       ),
     );
   }
 }
 
-// ignore: use_key_in_widget_constructors
-class HomePage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String _changetext = "Click below to change me";
-
-  void changetext() {
-    setState(() {
-      if (_changetext == "Click below to change me") {
-        _changetext = "hello! i have changed!";
-      } else {
-        _changetext = "Click below to change me";
-      }
-    });
-  }
-
-  Widget _bodyWidget() {
-    // ignore: avoid_unnecessary_containers
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                _changetext,
-                style: TextStyle(fontSize: 25.0),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              // ignore: deprecated_member_use
-              RaisedButton(
-                color: Colors.deepPurple,
-                child: const Text(
-                  "Click Me!",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                // onPressed: () {
-                //   if (_changetext == "Click below to change me") {
-                //     setState(() {
-                //       _changetext = "hello! i m changed!";
-                //     });
-                //   } else
-                //   if (_changetext == "hello! i m changed!"){
-                //    setState(() {
-                //       _changetext = "Click below to change me";
-                //    });
-                //   }
-                // }
-                onPressed: changetext,
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("change text"),
-      ),
-      body: _bodyWidget(),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.edit),
-        onPressed: () {
-          changetext();
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Login Page"),
+        ),
+        body: Stack(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        "assets/images/Unknown.png",
+                        fit: BoxFit.cover,
+                        // color: Colors.deepPurple,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            fillColor: Colors.deepPurple[200],
+                            labelText: "Username",
+                            hintText: "Enter username",
+                            hintMaxLines: 12,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              labelText: "Email-Id",
+                            hintText: "Enter you email-id",),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              labelText: "Password",
+                            hintText: "Enter password",),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        // ignore: deprecated_member_use
+                        RaisedButton(onPressed: (){},
+                        child: const Text("Login",style: TextStyle(color: Colors.white)),
+                        color: Colors.green,)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
