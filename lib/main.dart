0cece3d5b2767ddf65d2d1ca078a5cbc676098d3
Pawan Kumar <pawan.kumar.ece20@itbhu.ac.in>
2021-06-22 15:33:28 +0530

    Chapter-1 end

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("hello world")));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My First App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Page"),
+        ),
+        body: const Center(
+          child: Text("hello world!I am pawan"),
+        ),
+      ),
+    );
+  }
 }
