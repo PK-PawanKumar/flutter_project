c738a3824e3d4798f6e3ccc40ae2ec551d6c888c
Pawan Kumar <pawan.kumar.ece20@itbhu.ac.in>
2021-06-24 15:28:57 +0530

    Chapter-10 end;,title-listview

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,21 +8,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final message = ['pawan', 'sawan', 'rohan', 'sohan'];
-  final message2 = ['pawan', 'sawan', 'rohan', 'sohan'];
+  final message = [];
+  final message2 = [];
+  final message3 = [];
+  final message4 = [];
+  final deadline = [];
+  final alarmcolor = [];
+  final CheckboxStatus = [];
   final messagecontroller = TextEditingController();
+  final message3controller = TextEditingController();
+  final datecontroller = TextEditingController();
   // ignore: prefer_final_fields
   var _iconColor = Colors.red;
+
+  @override
+  void initState() {
+    setState(() {
+      // alarmcolor.length = message.length + 1;
+      for (int i = 0; i < alarmcolor.length; i++) {
+        alarmcolor[i] = Colors.red;
+        CheckboxStatus[i] = Colors.red;
+      }
+    });
+  }
 
   void addItemToList() {
     setState(() {
       message.insert(0, messagecontroller.text);
       message2.insert(0, messagecontroller.text);
+      message3.insert(0, message3controller.text);
+      message4.insert(0, message3controller.text);
+      deadline.insert(0, datecontroller.text);
+      messagecontroller.text = '';
+      message3controller.text = '';
+      datecontroller.text = '';
+      alarmcolor.insert(0, Colors.red);
+      CheckboxStatus.insert(0, Colors.red);
+      alarmcolor.length++;
+      CheckboxStatus.length++;
     });
   }
 
-  
+  void deleteItemfromList(int index) {
+    setState(() {
+      message.remove(message[index]);
+    });
+  }
 
+  void Editdetails(int index) {}
 
   // List _mylist = <String>[];
   @override
@@ -31,103 +64,175 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("my list"),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: messagecontroller,
-            decoration: InputDecoration(
-                labelText: "Add Elemets", hintText: "Enter Element Details"),
-          ),
-          IconButton(
-            onPressed: () {
-              addItemToList();
-            },
-            icon: const Icon(Icons.add),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: message.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 4),
-                  child: Card(
-                    
-                    color: (index % 2 == 0)
-                        ? Colors.deepOrange[100]
-                        : Colors.deepPurple[100],
-                    child: Column(
-                      children: [
-                        ListTile(
-                          leading: const Icon(Icons.task),
-                          title: Text(
-                            message[index],
-                            style: TextStyle(
-                              color: Colors.red,
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: messagecontroller,
+              decoration: InputDecoration(
+                  labelText: "Add Title", hintText: "Enter title "),
+            ),
+            TextField(
+              controller: message3controller,
+              decoration: InputDecoration(
+                  labelText: "Add Subtitle", hintText: "Enter subtitals"),
+            ),
+            TextField(
+              controller: datecontroller,
+              decoration: InputDecoration(
+                  labelText: "Deadline", hintText: "Enter date"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Text("Add task"),
+                    IconButton(
+                      onPressed: () {
+                        addItemToList();
+                      },
+                      icon: const Icon(Icons.add),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: message.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    child: Card(
+                      color: (index % 2 == 0)
+                          ? Colors.deepOrange[100]
+                          : Colors.deepPurple[100],
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: const Icon(
+                              Icons.task,
+                              size: 40,
+                            ),
+                            title: Row(
+                              children: [
+                                Text(
+                                  message[index],
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 40,
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.date_range,
+                                    color: Colors.blue,
+                                    size: 25,
+                                  ),
+                                ),
+                                Text(
+                                  deadline[index],
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            subtitle: Text(
+                              message3[index],
+                              style: TextStyle(
+                                color: Colors.red,
+                              ),
                             ),
                           ),
-                        ),
-                        Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              IconButton(
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        Editdetails(index);
+                                      });
+                                    },
+                                    icon: const Icon(Icons.edit)),
+                                // IconButton(
+                                //     onPressed: () {
+                                //       setState(() {
+                                //         message[index] = message2[index];
+                                //         message3[index] = message4[index];
+                                //       });
+                                //     },
+                                //     icon: const Icon(Icons.refresh,
+                                //         color: Colors.blue)),
+                                IconButton(
                                   onPressed: () {
                                     setState(() {
-                                      // message[index] = 'deleated';
+                                      message[index] = 'deleted';
+                                      message3[index] = '';
+                                      deleteItemfromList(index);
                                     });
                                   },
-                                  icon: const Icon(Icons.edit)),
-                              IconButton(
+                                  icon: const Icon(Icons.delete,
+                                      color: Colors.red),
+                                ),
+                                IconButton(
                                   onPressed: () {
                                     setState(() {
-                                      message[index] = message2[index];
+                                      if (alarmcolor[index] == Colors.red) {
+                                        alarmcolor[index] = Colors.green;
+                                      } else {
+                                        alarmcolor[index] = Colors.red;
+                                      }
                                     });
                                   },
-                                  icon: const Icon(Icons.refresh,
-                                      color: Colors.blue)),
-                              IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    message[index] = 'deleted';
-                                  });
-                                },
-                                icon: const Icon(Icons.delete, color: Colors.red),
-                              ),
-                              IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    if(_iconColor == Colors.red) {
-                                      _iconColor = Colors.green;
-                                    } else{
-                                          _iconColor = Colors.red;
-                                        }
-                                  });
-                                },
-                                icon: Icon(Icons.alarm,
-                                color: _iconColor,
-                                size: 30,),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
+                                  icon: Icon(
+                                    Icons.alarm,
+                                    color: alarmcolor[index],
+                                    size: 30,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      if (CheckboxStatus[index] == Colors.red) {
+                                        CheckboxStatus[index] = Colors.green;
+                                      } else {
+                                        CheckboxStatus[index] = Colors.red;
+                                      }
+                                    });
+                                  },
+                                  icon: Icon(
+                                    Icons.check_box,
+                                    color: CheckboxStatus[index],
+                                    size: 30,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   child: const Icon(Icons.add),
-      //   onPressed: () {
-      //     setState(() {
-      //       message.insert(message.length++, 'hello');
-      //       message2.insert(message.length++, 'hello');
-      //     });
-      //   },
-      // ),
     );
   }
 }
